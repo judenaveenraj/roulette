94b6b8ee9781ad84ad0ae5312a64032df1d5a5e6
judenaveenraj <judenaveenraj@yahoo.co.in>
2011-08-22 19:49:48 +0530

    Wheel rotation done.. working on stop animate

--- myscript.js
+++ myscript.js
@@ -16,9 +16,40 @@ var circ=canvas.display.image({
 x:250,
 y:canvas.height/2,
 origin: {x:"center",y:"center"},
-image: "wheel.jpg"
+image: "wheel.jpg",
+rotation:0
 });
 
-set 
+function stopspin(){
+canvas.timeline.stop();
+}
+
+var spinbtn = canvas.display.rectangle({
+x:500,
+y:400,
+width:100,
+height:50,
+fill: "#343",
+});
+var stopbtntext=canvas.display.text({
+x: spinbtn.width/2,
+y: spinbtn.height/2,
+origin: {x:"center", y:"center"},
+font: "bold 15px sans-serif",
+text:"Stop",
+fill: "#0aa",
+});
+
+
+spinbtn.bind("click tap",stopspin());
+spinbtn.addChild(stopbtntext);
+canvas.addChild(spinbtn);
+
+
+
+canvas.setLoop(function(){
+circ.rotation+=5;
+}).start();
+
 canvas.addChild(rect);
 canvas.addChild(circ);
