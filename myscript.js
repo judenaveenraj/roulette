8764e394faa85aa1c97473fec14df0068c75ad0d
judenaveenraj <judenaveenraj@yahoo.co.in>
2011-08-23 23:36:40 +0530

    Added ball rotation, wheel speed reduction randomised, ball radius increase

--- myscript.js
+++ myscript.js
@@ -1,11 +1,14 @@
 var spinwheel=1;
 var wheelspeed=5.0;
-var posx=0.0,posy=0.0,posrad=0.0,posangle=0.0;
-
+var posx=0.0,posy=0.0,posrad=0.0,posangrad=0.0,posangle=0.0;
+var ballspdinc=0.0;
+var dbgctr=0;
 /////////////////////////////////////////////////////////////
+var arr=[33,22,6,30,1,13,27,3,18,32,10,24,4,34,16,7,12,28,2,9,25,19,14,0,23,5,31,17,35,11,2,26,29,15,8,20];
 
 var canvas= oCanvas.create({
 canvas: "#canvas",
+background: "#0f0",
 fps:30
 });
 
@@ -17,6 +20,7 @@ width:canvas.width,
 height:canvas.height,
 stroke: " 10px #239 "
 });
+
 
 var wheel=canvas.display.image({
 x:250,
@@ -60,6 +64,12 @@ fill: "#0aa",
 
 //////////////////////////////////////////////////////////////
 
+
+function randomFromTo(from, to){
+       return Math.random() * (to - from + 1) + from;
+    }
+
+
 function reducespin(){
 if (wheelspeed>0)  setTimeout(function(){wheelspeed=(wheelspeed>0)? wheelspeed-0.005:0; reducespin();},5);
 else wheelspeed=0;
@@ -68,7 +78,27 @@ else wheelspeed=0;
 
 function stopspin(){
 spinwheel=0;
-reducespin();
+var t=randomFromTo(1,3)*1000;  //18
+setTimeout(function(){reducespin();},t);
+}
+
+function ballAccelrt(){
+dbgctr=0
+ballspdinc=1.6;
+console.log(" 1:"+posx+" 2:"+posy+" 3:"+posrad+" 4:"+posangrad+" 5:"+posangle+" 6:"+ball.x+" 7:"+ball.y);
+posangle+=ballspdinc;
+posangrad=posangle*(Math.PI/180);
+posx=posrad*Math.cos(posangrad);
+posy=posrad*Math.sin(posangrad);
+
+ball.x=posx+250;
+ball.y=posy+250;
+console.log(" 1:"+posx+" 2:"+posy+" 3:"+posrad+" 4:"+posangrad+" 5:"+posangle+" 6:"+ball.x+" 7:"+ball.y);
+setTimeout(function(){incrRadius();},5);
+if(dbgctr<=100)
+setTimeout(function(){ballAccelrt();},5);
+dbgctr++;
+
 }
 
 
@@ -79,13 +109,20 @@ ball.radius-=0.5;
 dropball();
 },75);
 
+else ballAccelrt();
+}
+
+function incrRadius(){
+if (posrad<130)
+	posrad+=0.4;
 }
 
 function getBallPos(){
 posx=ball.x-250;
 posy=ball.y-250;
 posrad=Math.sqrt(Math.pow(posx,2)+Math.pow(posy,2));
-posangle=Math.acos(posx/posrad)*(180/Math.PI);
+posangrad=Math.acos(posx/posrad);
+posangle=posangrad*(180/Math.PI);
 //stopbtntext.text="x:"+posx+" y:"+posy+" a:"+posangle;
 
 }
@@ -100,7 +137,7 @@ stopbtn.bind("click tap",function(){if(spinwheel!=0) stopspin();});
 
 
 ball.dragAndDrop({
-	move: function(){ getBallPos();stopbtntext.text="x:"+(posx)+" y:"+(posy)+" a:"+posangle;},
+	move: function(){ stopbtntext.text="x:"+(posx)+" y:"+(posy)+" a:"+posangle;},
 	end: function(){ 
 		stopbtntext.text="x:"+(posx)+" y:"+(posy)+" a:"+posangle;
 		dropball();
